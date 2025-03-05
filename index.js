8a07213f81dad6d0b649ed6e9c513c366a18cc65
Brian <okellobrian18@gmail.com>
2025-03-05 11:29:48 +0300

    Function Scope

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*") {
+    function compliment (attribute = "special") {
+        return `You are ${flair}${attribute}${flair}!`
+    }
+    return compliment;
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"))
